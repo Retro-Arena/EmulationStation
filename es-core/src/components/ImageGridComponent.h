ba5f7d6229f5f85564015b558e651a8f78346c2b
Stephenm64 <Stephenm64@users.noreply.github.com>
2018-09-26 19:55:42 -0700

    Revert "Replace folder bitmap with svg artwork."
    This reverts commit 5992e523dd75abf593feb9b88044c88c00fd797d.

--- es-core/src/components/ImageGridComponent.h
+++ es-core/src/components/ImageGridComponent.h
@@ -90,7 +90,7 @@ ImageGridComponent<T>::ImageGridComponent(Window* window) : IList<ImageGridData,
 	mEntriesDirty = true;
 	mLastCursor = 0;
 	mDefaultGameTexture = ":/blank_game.png";
-	mDefaultFolderTexture = ":/folder.svg";
+	mDefaultFolderTexture = ":/folder.png";
 
 	mSize = screen * 0.80f;
 	mMargin = screen * 0.07f;
